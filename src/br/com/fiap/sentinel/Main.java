414ffefb97289a808bd9f6b819e8582931d1d360
RodrigoTadeuF <rodrigotfonsecas@outlook.com>
2020-06-15 22:19:15 -0300

    adicionado primeira parte do menus

--- src/br/com/fiap/sentinel/Main.java
+++ src/br/com/fiap/sentinel/Main.java
@@ -24,11 +24,61 @@ public class Main {
             System.out.println("Digite 8 para liberar um paciente");
             System.out.println("Digite 0 para sair");
             opcao = sc.nextInt();
+            sc.nextLine();
 
             switch (opcao) {
                 case 1:
-                    break;
+                    String nome, responsavel, cidade, estado, cidades, estados;
 
+                    RedeHospitalar redeHospitalar = new RedeHospitalar();
+
+                    System.out.println("Digite o nome da rede: ");
+                    nome = sc.nextLine();
+                    redeHospitalar.setNome(nome);
+
+                    System.out.println("Digite o nome do responsável: ");
+                    responsavel = sc.nextLine();
+                    redeHospitalar.setResponsavel(responsavel);
+
+                    System.out.println("A rede atende mais de uma cidade?");
+                    cidade = sc.nextLine();
+                    cidade = cidade.toUpperCase();
+
+                    if(cidade.equals("SIM")) {
+                        while(cidade.equals("SIM")) {
+                            System.out.println("Digite o nome da cidade: ");
+                            cidades = sc.nextLine();
+                            redeHospitalar.adicionaCidade(cidades);
+                            System.out.println("A rede atende mais alguma cidade?");
+                            cidade = sc.nextLine();
+                            cidade = cidade.toUpperCase();
+                        }
+                    } else {
+                        System.out.println("Digite o nome da cidade: ");
+                        cidades = sc.nextLine();
+                        redeHospitalar.adicionaCidade(cidades);
+                    }
+
+                    System.out.println("A rede atende mais de um estado?");
+                    estado = sc.nextLine();
+                    estado = estado.toUpperCase();
+
+                    if(estado.equals("SIM")) {
+                        while(estado.equals("SIM")) {
+                            System.out.println("Digite o nome da estado: ");
+                            estados = sc.nextLine();
+                            redeHospitalar.adicionaEstado(estados);
+                            System.out.println("A rede atende mais alguma estado?");
+                            estado = sc.nextLine();
+                            estado = estado.toUpperCase();
+                        }
+                    } else {
+                        System.out.println("Digite o nome da estado: ");
+                        estados = sc.nextLine();
+                        redeHospitalar.adicionaCidade(estados);
+                    }
+                    break;
+/*
                 case 2:
                     break;
 
@@ -52,7 +102,7 @@ public class Main {
 
                 case 0:
                     break;
-
+*/
                 default:
                     System.out.println("Por favor, digite uma opção válida");
             }
--- src/br/com/fiap/sentinel/RedeHospitalar.java
+++ src/br/com/fiap/sentinel/RedeHospitalar.java
@@ -9,8 +9,57 @@ public class RedeHospitalar {
     private String responsavel;
     private List<String> cidadesCobertura = new ArrayList<String>();
     private List<String> estadosCobertura = new ArrayList<String>();
-    private String cidadeOrigem;
 
     private List<Hospital> hospitais = new ArrayList<Hospital>();
 
+    public RedeHospitalar() {  }
+
+    public RedeHospitalar(String nome, String responsavel, List<String> cidadesCobertura, List<String> estadosCobertura, List<Hospital> hospitais) {
+        this.nome = nome;
+        this.responsavel = responsavel;
+        this.cidadesCobertura = cidadesCobertura;
+        this.estadosCobertura = estadosCobertura;
+        this.hospitais = hospitais;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(String responsavel) {
+        this.responsavel = responsavel;
+    }
+
+    public List<String> getCidadesCobertura() {
+        return cidadesCobertura;
+    }
+
+    public List<String> getEstadosCobertura() {
+        return estadosCobertura;
+    }
+
+
+    public List<Hospital> getHospitais() {
+        return hospitais;
+    }
+
+    public void setHospitais(List<Hospital> hospitais) {
+        this.hospitais = hospitais;
+    }
+
+    public void adicionaCidade(String cidade) {
+        cidadesCobertura.add(cidade);
+    }
+
+    public void adicionaEstado(String estado) {
+        estadosCobertura.add(estado);
+    }
 }
